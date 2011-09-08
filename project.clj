89e0a3616fdb3c217bb884d2c1c05dde882a0872
Craig Andera <candera@wangdera.com>
2011-09-08 09:03:27 -0400

    [WIP] Added basic web app infrastructure

--- project.clj
+++ project.clj
@@ -2,5 +2,9 @@
   :description "FIXME: write description"
   :dependencies [[org.clojure/clojure "1.3.0-beta3"]
                  [clj-http "0.2.0"]
-                 [clj-json "0.4.0"]]
+                 [clj-json "0.4.0"]
+                 [ring/ring-jetty-adapter "0.3.11"]
+                 [ring/ring-core "0.3.11"]
+                 [compojure "0.6.5"]
+                 [enlive "1.0.0"]]
   :dev-dependencies [[swank-clojure "1.4.0-SNAPSHOT"]])
--- src/geirrod/core.clj
+++ src/geirrod/core.clj
@@ -1,9 +1,47 @@
 (ns geirrod.core
   (:require [clj-json.core :as json]
-            [clj-http.client :as http]))
+            [clj-http.client :as http]
+            [compojure.core :as compojure]
+            [compojure.route :as route]
+            [compojure.handler :as handler]
+            [ring.adapter.jetty :as jetty]))
 
 (defn- issues-list-url [user repo state]
   (str "https://github.com/api/v2/json/issues/list/" user "/" repo "/" state))
 
 (defn issues-list [user repo state]
   (json/parse-string (:body (http/get (issues-list-url user repo state)))))
+
+(defn to-html-str [input] (str input))
+
+(defn test-page [] "Hi!")
+
+(defn index [] "Hello!")
+
+(compojure/defroutes test-routes
+  (compojure/GET "/test" [] (test-page)))
+
+(compojure/defroutes page-routes
+  (compojure/GET "/" [] (to-html-str (index))))
+
+(compojure/defroutes default-routes
+  (route/resources "/")
+  (route/not-found "Page not found"))
+
+(compojure/defroutes all-routes
+  test-routes
+  (handler/site page-routes)
+  default-routes)
+
+;; (def app (wrap-params all-routes))
+
+(defn -main [& args]
+  (let [port (Integer/parseInt (get (System/getenv) "PORT" "8080"))]
+    (jetty/run-jetty app {:port port})))
+
+(let [server (atom nil)
+      set-server #(reset! server %)]
+  (defn debug-start []
+    (future (jetty/run-jetty (var app) {:port 8080 :configurator set-server})))
+  (defn debug-stop []
+    (.stop @server)))
