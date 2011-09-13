037a52c0f979f3700cb76e20709a957c4f78cdb9
Craig Andera <candera@wangdera.com>
2011-09-13 09:01:29 -0400

    Updating to Clojure 1.3.0-RC0

--- project.clj
+++ project.clj
@@ -1,6 +1,6 @@
 (defproject geirrod "1.0.0-SNAPSHOT"
   :description "FIXME: write description"
-  :dependencies [[org.clojure/clojure "1.3.0-beta3"]
+  :dependencies [[org.clojure/clojure "1.3.0-RC0"]
                  [clj-http "0.2.0"]
                  [clj-json "0.4.0"]
                  [ring/ring-jetty-adapter "0.3.11"]
