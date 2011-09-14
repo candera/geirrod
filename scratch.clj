19a1e867c9401d800b7982674606f0e1f96d1d5d
Craig Andera <candera@wangdera.com>
2011-09-13 22:29:16 -0400

    Updated scratch.clj

--- scratch.clj
+++ scratch.clj
@@ -3,3 +3,14 @@
 (http/get "http://google.com")
 (http/get "https://github.com")         ; This fails under slime, but
                                         ; works at the REPL. WTF?
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+(import [java.net URL URLConnection])
+(import [java.io BufferedReader InputStreamReader])
+(let [conn (.openConnection (URL. "http://wangdera.com"))
+      in (BufferedReader. (InputStreamReader. (.getInputStream conn)))]
+  (dotimes [i 100]
+    (let [line (.readLine in)]
+      (if (zero? (mod i 2))
+        (println i line)))))
