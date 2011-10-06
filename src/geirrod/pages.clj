da5a75353823db5fe12c79bfd3521e1e33de3da5
Craig Andera <candera@wangdera.com>
2011-10-06 19:46:23 -0400

    #10 WIP Impement category selectors

--- src/geirrod/pages.clj
+++ src/geirrod/pages.clj
@@ -18,31 +18,35 @@
   [:ol]
   (enlive/content (map issue-box issues)))
 
-(enlive/defsnippet category-select "html/snippets.html" [:.category-select] [categories]
+(enlive/defsnippet category-select "html/snippets.html" [:#category-select] [categories]
   [:option]
   (enlive/clone-for [category categories]
                     (enlive/do->
                      (enlive/content category)
                      (enlive/set-attr :value category))))
 
-(enlive/deftemplate issues-page "html/issues.html" [categories]
+(enlive/defsnippet grid "html/snippets.html" [:#issue-grid] [lanes issues-by-lane]
+  (enlive/content (map #(lane % (get issues-by-lane %)) lanes)))
+
+(enlive/deftemplate issues-page "html/issues.html" [content]
   [:body]
-  (enlive/append (category-select categories)))
+  (enlive/append content))
 
-;; (enlive/deftemplate issues-page "html/issues.html" [issues-by-lane lanes]
-;;   [:#lanes]
-;;   (enlive/content (map #(lane % (get issues-by-lane %)) lanes)))
+;; TODO: Consider something more flexible than a cond as this gets
+;; more complicated
+(defn- issues-content [account repo display category]
+  (let [labels (labels account repo)
+        label-names (label-names labels)]
+   (cond
+    (= display "grid")
+    (let [issues (issues account repo "open")
+          lanes (lanes category label-names)
+          issues-by-lane (group-by-lane category issues)]
+      (grid lanes issues-by-lane))
 
-;; (defn issues-html [account repo]
-;;   (let [issues (issues account repo "open")
-;;         labels (labels account repo)
-;;         label-names (label-names labels)
-;;         lanes (lanes "status" label-names)
-;;         issues-by-lane (group-by-lane "status" issues)]
-;;     (apply str (issues-page issues-by-lane lanes))))
+    :else
+    (category-select (categories label-names)))))
 
 (defn issues-html [account repo params]
-  (let [labels (labels account repo)
-        label-names (label-names labels)
-        categories (categories label-names)]
-    (issues-page categories)))
+  (let [{display :display category :group} params]
+    (apply str (issues-page (issues-content account repo display category)))))
