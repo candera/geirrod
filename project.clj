2a17e6aed1b5e4908fc8530d5103c75988404de0
Craig Andera <candera@wangdera.com>
2011-10-07 11:49:57 -0400

    Update project dependencies

--- project.clj
+++ project.clj
@@ -1,8 +1,8 @@
 (defproject geirrod "1.0.0-SNAPSHOT"
   :description "FIXME: write description"
   :dependencies [[org.clojure/clojure "1.3.0"]
-                 [clj-http "0.2.0"]
-                 [clj-json "0.4.0"]
+                 [clj-http "0.2.1"]
+                 [clj-json "0.4.3"]
                  [ring/ring-jetty-adapter "0.3.11"]
                  [ring/ring-core "0.3.11"]
                  [compojure "0.6.5"]
