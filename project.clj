67ef178b6e64f9dcd66e7e6327c915ffd322db5f
Craig Andera <candera@wangdera.com>
2011-10-07 15:52:11 -0400

    ORevert "Downgrading to 1.2.1 in the hope that I can deploy to Heroku"
    This reverts commit e06360f3d1c03902c449b5a887c8fa1494e114cb.

--- project.clj
+++ project.clj
@@ -1,6 +1,6 @@
 (defproject geirrod "1.0.0-SNAPSHOT"
   :description "FIXME: write description"
-  :dependencies [[org.clojure/clojure "1.2.1"]
+  :dependencies [[org.clojure/clojure "1.3.0"]
                  [clj-http "0.2.1"]
                  [clj-json "0.4.3"]
                  [ring/ring-jetty-adapter "0.3.11"]
